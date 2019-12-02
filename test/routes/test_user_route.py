e00bf616d8b433624436ed5993f9c9bbb2366808
Thomas Junk <thomas.junk@intevation.de>
2019-12-02 13:43:01 +0100

    Further user route tests

--- test/routes/test_user_route.py
+++ test/routes/test_user_route.py
@@ -7,18 +7,23 @@ import pytest
 import falcon
 
 from routes import User
+from user import UserExists
 
 mock_service = mock.Mock()
 log = mock.Mock()
 user_route = User(mock_service, log)
 resp = mock.Mock()
 
+login = "test"
+password = "test"
+groups = ["user"]
+
 
 def get_user():
     user = {
-        "login": "test",
-        "password": "test",
-        "groups": ["user"]
+        "login": login,
+        "password": password,
+        "groups": groups
     }
     return user
 
@@ -31,6 +36,10 @@ def build_request(session):
     return req
 
 
+def build_request_with_user():
+    return build_request(session={"user": get_user()})
+
+
 class TestUserRoutes:
     def test_list_users_unauthorized(self):
         with pytest.raises(falcon.errors.HTTPUnauthorized):
@@ -38,7 +47,30 @@ class TestUserRoutes:
             user_route.on_get(req, resp)
 
     def test_list_users(self):
-        req = build_request(session={"user": get_user()})
+        req = build_request_with_user()
         mock_service.list.return_value = [get_user()]
         user_route.on_get(req, resp)
         assert len(resp.media) == 1
+
+    def test_create_user_empty_request(self):
+        req = build_request_with_user()
+        req.media = {}
+        user_route.on_post(req, resp)
+        assert resp.status == falcon.HTTP_400
+
+    def test_create_user(self):
+        req = build_request_with_user()
+        req.media = {"user": get_user()}
+        item = mock.Mock()
+        item.to_dict.return_value = get_user()
+        mock_service.create_new_user.return_value = item
+        user_route.on_post(req, resp)
+        assert resp.media is not None
+        assert resp.media["login"] == login
+
+    def test_create_user_exits(self):
+        req = build_request_with_user()
+        req.media = {"user": get_user()}
+        mock_service.create_new_user.side_effect = UserExists()
+        user_route.on_post(req, resp)
+        assert resp.status == falcon.HTTP_409
